f0be5e5fae47545209eb703edbe29b2aee89ebdb
Sandeep Kumar Mamman <skmamman@outlook.com>
2021-03-06 14:54:15 +0200

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import Aos from 'aos';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import Particles from './Components/Particles/Particle';
+import Display from './Components/Display/Display';
+import About from './Components/About/About';
+import Skill from './Components/Skill/Skill';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Particles />
+        <Navigation />
+        <Logo />
+        <Display id="display"/>
+        <About id="about"/>
+        <Skill id="skill"/>
     </div>
   );
 }
